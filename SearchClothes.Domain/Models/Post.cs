4c19708eac2b1f301aecaf63ee1a60492a492c46
Chinaskihate <islam.kadyrov.dlschool@gmail.com>
2022-02-17 18:18:34 +0300

    Add project files.

---
+++ SearchClothes.Domain/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchClothes.Domain.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public DateTime LastEditTime { get; set; }
+    }
+}
